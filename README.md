e7b820e0d4d06ec23ef0fec8e4fe2c432d67e050
Félix L. Morales <12535179+morales-felix@users.noreply.github.com>
2024-05-12 15:47:31 -0500

    Prettifying notebook

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# Simulating the Qatar 2022 FIFA World Cup
+# Simulating the Qatar 2022 FIFA World Cup  
 
-As a fun project that can develop some code for an upcoming personal project, I undertook this simulation to fill out some World Cup bracket challenges.
+## Introduction
 
-It is heavily based upon Elo ratings found on https://eloratings.net as a measure of relative team strength and to update this team strength measure every time a simulated game is played.  
-Code to implement the Elo rating system is based on FiveThirtyEight's NFL forecasting game (https://github.com/tulachin/nfl-elo-game).
+Needless to say, I am a soccer fan (not too crazy though... As I continue to live on this Earth, I realize that fandom isn't a static thing). So this was a fun project which also helped me to start developing code for future personal project of mine. Full disclosure: One big motivation to do this was a World Cup bracket challenge that I played with my coworkers. As far as I can tell, I was the only one who did this sort of thing. Did I win the bracket? Read on 😉
+
+This simulation is heavily based upon Elo ratings found on <https://eloratings.net> as a measure of relative team strength and to update this team strength measure every time a simulated game is played.
+Code to implement the Elo rating system is based on FiveThirtyEight's NFL forecasting game (<https://github.com/morales-felix/nfl-elo-game>).
 
 Notes on Elo implementation:  
--Per https://eloratings.net/about, the K constant is set to 60 as this is a World Cup competition.    
--Probabilities given by the Elo rating system are binary. I came up with a workaround to convert them to ternary probabilities given that association football (a.k.a. soccer) admits three outcomes after a match (win, tie, lose).  
--I did not simulate scorelines. Rather, I simply used probabilities to decide whether a team would win, tie, or lose. As such, I did not use the goal difference multiplier specified in https://eloratings.net/about  
--I'll be happy to talk about the workaround, but I wouldn't take it as gospel. There might be ways to do this, but I did not research it. Wanted to have fun, not produce an academic-paper-worthy method, nor a sellable product.  
--In the end, results aren't that different from other more publicized methods. Brazil is a strong team... always.
 
-LEARNINGS: I should have put my money where my model was, and bet for ARGENTINA!!! It was predicting, narrowly, that Argentina would win the World Cup. It did so when I simulated the playoffs (i.e. not at the very beginning of the competition), which I did to get points in a bracket I played at work. Dang!!!
+- Per <https://eloratings.net/about>, the K constant is set to 60 as this is a World Cup competition.  
+- Probabilities given by the Elo rating system are binary. I came up with a workaround to convert them to ternary probabilities given that association football (a.k.a. soccer) admits three outcomes after a match (win, tie, lose).  
+- I did not simulate scorelines. Rather, I simply used probabilities to decide whether a team would win, tie, or lose. As such, I did not use the goal difference multiplier specified in <https://eloratings.net/about>  
+- I'll be happy to talk about the workaround, but I wouldn't take it as gospel. There might be ways to do this, but I did not research it. Wanted to have fun, not produce an academic-paper-worthy method, nor a sellable product.  
+- In the end, results aren't that different from other more publicized methods. Brazil is a strong team... always.  
+
+## Do you want to use this notebook?  
+
+### Prerequisites  
+
+You need to have Python and pip installed on your system. You can download Python from the official website and pip is included in the Python installation. However, I recommend using the Anaconda distribution of Python which includes pip and other useful tools.  
+
+### Installation  
+
+1. Clone the repository to your local machine.  
+2. Navigate to the project directory.  
+3. Create a fresh conda environment, and run the following:  
+`pip install -r requirements.txt`  
+
+If you run into any issues with the installation, try deleting Windows-specific packages from the `requirements.txt` file.
--- src/world_cup_simulator.py
+++ src/world_cup_simulator.py
@@ -1,8 +1,8 @@
-import numpy as np
-import pandas as pd
 import csv
 import math
 import random
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -10,67 +10,74 @@ def read_games(file):
     """
     Initializes game objects from csv
     """
-        
-    games = [item for item in csv.DictReader(open(file, encoding='latin-1'))]
-    
+
+    games = [item for item in csv.DictReader(open(file, encoding="latin-1"))]
+
     return games
 
 
 def simulate_group_stage_game(game, ternary=True):
-    
-    home = game['elo_prob_home']
-    away = 1 - game['elo_prob_home']
+    """
+    Simulates a single game in the group stage
+    """
+
+    home = game["elo_prob_home"]
+    away = 1 - game["elo_prob_home"]
     tie = 0
-    
+
     # Simulating game proper
     wildcard = random.uniform(0, 1)
-    
+
     # Concoction to go from binary probabilities to ternary
     if ternary:
         if home > 0 and home < 1:
-            home_odds = home/away
+            home_odds = home / away
             tie_odds = 1
-            away_odds = 1 - abs(home - 0.5)*2
-    
-            home_odds1 = (home/away)/min(away_odds, tie_odds, home_odds)
-            tie_odds1 = 1/min(away_odds, tie_odds, home_odds)
-            away_odds1 = (1 - abs(home - 0.5)*2)/min(away_odds, tie_odds, home_odds)
-    
-            home = home_odds1/(home_odds1+tie_odds1+away_odds1)
-            tie = tie_odds1/(home_odds1+tie_odds1+away_odds1)
-            away = away_odds1/(home_odds1+tie_odds1+away_odds1)
-    
+            away_odds = 1 - abs(home - 0.5) * 2
+
+            home_odds1 = (home / away) / min(away_odds, tie_odds, home_odds)
+            tie_odds1 = 1 / min(away_odds, tie_odds, home_odds)
+            away_odds1 = (1 - abs(home - 0.5) * 2) / min(away_odds, tie_odds, home_odds)
+
+            home = home_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+            tie = tie_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+            away = away_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+
         elif home == 0:
             tie = 0
             away = 1
-    
+
         elif home == 1:
             tie = 0
             away = 0
-    
+
         else:
             raise ValueError("Probabilities must be floats between 0 and 1, inclusive")
     else:
         pass
-        
-    
+
     if wildcard >= 0 and wildcard < away:
         return 0
-    elif wildcard >= away and wildcard < away+tie and ternary:
+
+    if wildcard >= away and wildcard < away + tie and ternary:
         return 0.5
-    elif wildcard >= away+tie and wildcard <= 1:
+
+    if wildcard >= away + tie and wildcard <= 1:
         return 1
-    
-    
+
+
 def simulate_playoff_game(game, ternary=True):
-    
-    home = game['elo_prob_home']
-    away = 1 - game['elo_prob_home']
+    """
+    Simulates a single game in the knockout stage
+    """
+
+    home = game["elo_prob_home"]
+    away = 1 - game["elo_prob_home"]
     tie = 0
-    
+
     # Simulating game proper
     wildcard = random.uniform(0, 1)
-    
+
     # Concoction to go from binary probabilities to ternary
     # 50-50% should translate into 1/3, 1/3, 1/3 (even split of probability space)
     # With increasing lopsidedness (e.g. 75-25%), the stronger team should see increased win probability,
@@ -78,150 +85,160 @@ def simulate_playoff_game(game, ternary=True):
     # as lopsidedness increases, but I assume that at a lower rate than weaker team win probability.
     if ternary:
         if home > 0 and home < 1:
-            home_odds = home/away
+            home_odds = home / away
             tie_odds = 1
-            away_odds = 1 - abs(home - 0.5)*2
-    
-            home_odds1 = (home/away)/min(away_odds, tie_odds, home_odds)
-            tie_odds1 = 1/min(away_odds, tie_odds, home_odds)
-            away_odds1 = (1 - abs(home - 0.5)*2)/min(away_odds, tie_odds, home_odds)
-    
-            home = home_odds1/(home_odds1+tie_odds1+away_odds1)
-            tie = tie_odds1/(home_odds1+tie_odds1+away_odds1)
-            away = away_odds1/(home_odds1+tie_odds1+away_odds1)
-    
+            away_odds = 1 - abs(home - 0.5) * 2
+
+            home_odds1 = (home / away) / min(away_odds, tie_odds, home_odds)
+            tie_odds1 = 1 / min(away_odds, tie_odds, home_odds)
+            away_odds1 = (1 - abs(home - 0.5) * 2) / min(away_odds, tie_odds, home_odds)
+
+            home = home_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+            tie = tie_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+            away = away_odds1 / (home_odds1 + tie_odds1 + away_odds1)
+
         elif home == 0:
             tie = 0
             away = 1
-    
+
         elif home == 1:
             tie = 0
             away = 0
-    
+
         else:
             raise ValueError("Probabilities must be floats between 0 and 1, inclusive")
     else:
         pass
-        
-    
+
     if wildcard >= 0 and wildcard < away:
-        return game['away_team'], game['home_team'], 0, False
-    
-    elif wildcard >= away and wildcard < away+tie and ternary:
+        return game["away_team"], game["home_team"], 0, False
+
+    if wildcard >= away and wildcard < away + tie and ternary:
         # Simulating outcome of a penalty shootout. I assume it is a coin-toss. An advancing team is needed.
-        teams = [game['away_team'], game['home_team']]
+        teams = [game["away_team"], game["home_team"]]
         advances = np.random.choice(teams)
         teams.remove(advances)
         return advances, teams[0], 0.5, True
-    
-    elif wildcard >= away+tie and wildcard <= 1:
-        return game['home_team'], game['away_team'], 1, False
-    
+
+    if wildcard >= away + tie and wildcard <= 1:
+        return game["home_team"], game["away_team"], 1, False
+
 
 def simulate_group_stage(games, teams, ternary=True):
-        
+    """
+    Simulates the entire group stage
+    """
+
     for game in games:
-        team1, team2 = teams[game['home_team']], teams[game['away_team']]
-        
+        team1, team2 = teams[game["home_team"]], teams[game["away_team"]]
+
         # Home field advantage is BS
-        elo_diff = team1['rating'] - team2['rating']
+        elo_diff = team1["rating"] - team2["rating"]
 
         # This is the most important piece, where we set my_prob1 to our forecasted probability
-        game['elo_prob_home'] = 1.0 / (math.pow(10.0, (-elo_diff/400.0)) + 1.0)
+        game["elo_prob_home"] = 1.0 / (math.pow(10.0, (-elo_diff / 400.0)) + 1.0)
 
         # If game was played, maintain team Elo ratings
-        if game['result_home'] == "":
-            
-            game['result_home'] = simulate_group_stage_game(game, ternary)
-            
+        if game["result_home"] == "":
+
+            game["result_home"] = simulate_group_stage_game(game, ternary)
+
             # Elo shift based on K
-            shift = 60.0*(game['result_home'] - game['elo_prob_home'])
+            shift = 60.0 * (game["result_home"] - game["elo_prob_home"])
 
             # Apply shift
-            team1['rating'] += shift
-            team2['rating'] -= shift
+            team1["rating"] += shift
+            team2["rating"] -= shift
 
             # Apply points
-            if game['result_home'] == 0:
-                team1['points'] += 0
-                team2['points'] += 3
-            elif game['result_home'] == 0.5:
-                team1['points'] += 1
-                team2['points'] += 1
+            if game["result_home"] == 0:
+                team1["points"] += 0
+                team2["points"] += 3
+            elif game["result_home"] == 0.5:
+                team1["points"] += 1
+                team2["points"] += 1
             else:
-                team1['points'] += 3
-                team2['points'] += 0
+                team1["points"] += 3
+                team2["points"] += 0
 
-                
+
 def simulate_playoffs(games, teams, ternary=True):
-    
+    """
+    Simulates the entire knockout stage
+    """
+
     for game in games:
-        team1, team2 = teams[game['home_team']], teams[game['away_team']]
-        
+        team1, team2 = teams[game["home_team"]], teams[game["away_team"]]
+
         # Home field advantage is B.S. in modern soccer
-        elo_diff = team1['rating'] - team2['rating']
+        elo_diff = team1["rating"] - team2["rating"]
 
         # This is the most important piece
-        game['elo_prob_home'] = 1.0 / (math.pow(10.0, (-elo_diff/400.0)) + 1.0)
+        game["elo_prob_home"] = 1.0 / (math.pow(10.0, (-elo_diff / 400.0)) + 1.0)
 
         # If game was played, maintain team Elo ratings
-        if game['advances'] == "" or game['loses'] == "":
-            
-            game['advances'], game['loses'], game['result_home'], game['penalties'] = simulate_playoff_game(game, ternary)
-            
+        if game["advances"] == "" or game["loses"] == "":
+
+            game["advances"], game["loses"], game["result_home"], game["penalties"] = (
+                simulate_playoff_game(game, ternary)
+            )
+
             # Elo shift based on K
-            shift = 60.0*(game['result_home'] - game['elo_prob_home'])
+            shift = 60.0 * (game["result_home"] - game["elo_prob_home"])
 
             # Apply shift
-            team1['rating'] += shift
-            team2['rating'] -= shift
-            
-            
+            team1["rating"] += shift
+            team2["rating"] -= shift
+
         # This is to populate quarterfinals and the such depending on previous rounds' results
-        next_stage = int(game['to_match'])
-        
+        next_stage = int(game["to_match"])
+
         if next_stage < 14:
-            
-            if games[next_stage]['home_team'] == "":
-                games[next_stage]["home_team"] = game['advances']
+
+            if games[next_stage]["home_team"] == "":
+                games[next_stage]["home_team"] = game["advances"]
             else:
-                games[next_stage]["away_team"] = game['advances']
-                
+                games[next_stage]["away_team"] = game["advances"]
+
         elif next_stage == 14:
-            if games[next_stage]['home_team'] == "":
-                games[next_stage]["home_team"] = game['advances']
+            if games[next_stage]["home_team"] == "":
+                games[next_stage]["home_team"] = game["advances"]
             else:
-                games[next_stage]["away_team"] = game['advances']
-                
-            if games[next_stage+1]['home_team'] == "":
-                games[next_stage+1]["home_team"] = game['loses']
+                games[next_stage]["away_team"] = game["advances"]
+
+            if games[next_stage + 1]["home_team"] == "":
+                games[next_stage + 1]["home_team"] = game["loses"]
             else:
-                games[next_stage+1]["away_team"] = game['loses']
-                
+                games[next_stage + 1]["away_team"] = game["loses"]
+
         else:
             pass
-                
-def collect_playoff_results(team, dataframe):
-    f = dataframe['home_team'] == team
-    g = dataframe['away_team'] == team
 
-    team_pd = dataframe.loc[f|g]
-    max_match_number = team_pd['match'].astype(int).max()
+
+def collect_playoff_results(team, dataframe):
+    """
+    Collects the results of a team in the knockout stage
+    """
+    
+    f = dataframe["home_team"] == team
+    g = dataframe["away_team"] == team
+
+    team_pd = dataframe.loc[f | g]
+    max_match_number = team_pd["match"].astype(int).max()
 
     if max_match_number < 8:
-        result = 'Round_of_16'
+        result = "Round_of_16"
     elif max_match_number >= 8 and max_match_number < 12:
-        result = 'Quarterfinals'
+        result = "Quarterfinals"
     elif max_match_number == 14:
-        if team_pd.loc[max_match_number, 'advances'] == team:
-            result = 'Champion'
+        if team_pd.loc[max_match_number, "advances"] == team:
+            result = "Champion"
         else:
-            result = 'Second_place'
+            result = "Second_place"
     elif max_match_number == 15:
-        if team_pd.loc[max_match_number, 'advances'] == team:
-            result = 'Third_place'
+        if team_pd.loc[max_match_number, "advances"] == team:
+            result = "Third_place"
         else:
-            result = 'Fourth_place'
-            
+            result = "Fourth_place"
+
     return result
-    
